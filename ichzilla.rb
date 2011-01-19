c207430ac82eeb965e45024b594769c6896b0086
MrBongiolo <ralfsb2008@gmail.com>
2011-01-19 17:40:31 -0400

    We already can calculate character HP

--- ichzilla.rb
+++ ichzilla.rb
@@ -1,6 +1,49 @@
 require "rubygems"
 require "sinatra"
+require "erb"
+
+helpers do
+  def select_tag(name, collection)
+    result = "<select name=\"#{name.to_s}\">\n"
+    collection.each do |c|
+      result += "<option value=\"#{c.to_s.downcase}\">#{c.to_s.capitalize}</option>\n"
+    end
+    result += "</select>\n"
+  end
+end
 
 get '/' do 
+  @classes = %w(warrior defender priest cleric
+                enchanter sorcerer archer assassin
+                life shadow rune
+                templar attacker gunner)
   erb :ichzilla
+end
+
+post '/hp' do
+  row_class = params[:row_class]
+  lvl = params[:lvl].to_i
+  con = params[:con].to_i
+  dex = params[:dex].to_i
+  str = params[:str].to_i
+  base_hp = (150 + (25 * lvl) + ((con-20) * 0.15 * lvl) + ((dex-20) * 0.05 *lvl))
+  row_class_hp = 0
+  case row_class
+    when 'warrior', 'attacker'
+      row_class_hp = ((con - 20) * 0.35 * lvl)
+    when 'defender', 'templar'
+      row_class_hp = ((con - 20) * 0.4 * lvl)
+    when 'assassin', 'archer', 'gunner', 'shadow'
+      row_class_hp = ((str - 20) * 0.1 * lvl)
+    when 'sorcerer', 'enchanter', 'rune'
+      row_class_hp = ((dex - 20) * 0.3 * lvl)
+    when 'priest', 'cleric'
+      row_class_hp = ((con - 20) * 0.2 * lvl)
+    when 'life'
+      row_class_hp = ((dex - 20) * 0.2 * lvl)
+    else
+      #nothin
+  end
+  @total_hp = base_hp + row_class_hp
+  erb :hp
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,67 @@
+//
 //Ichzilla Javascript file
+//
+
+function calculateHP() {
+  var values = {};
+  $.each($('form').serializeArray(), function(i, field) {
+    values[field.name] = field.value;
+  });
+  var base_hp = Math.floor(150 + (25 * values['level']) + ((values['con'] -20) * 0.15 * values['level']) + ((values['dex'] - 20) * 0.05 * values['level']));
+  var row_class_hp = 0;
+  switch(values['row_class']) {
+  case 'warrior':
+    row_class_hp = Math.floor((values['con'] -20) * 0.35 * values['level']);
+    break;
+  case 'attacker':
+    row_class_hp = Math.floor((values['con'] -20) * 0.35 * values['level']);
+    break;
+  case 'defender':
+    row_class_hp = Math.floor((values['con'] -20) * 0.4 * values['level']);
+    break;
+  case 'templar':
+    row_class_hp = Math.floor((values['con'] -20) * 0.4 * values['level']);
+    break;
+  case 'assassin':
+    row_class_hp = Math.floor((values['str'] -20) * 0.1 * values['level']);
+    break;
+  case 'archer':
+    row_class_hp = Math.floor((values['str'] -20) * 0.1 * values['level']);
+    break;
+  case 'gunner':
+    row_class_hp = Math.floor((values['str'] -20) * 0.1 * values['level']);
+    break;
+  case 'shadow':
+    row_class_hp = Math.floor((values['str'] -20) * 0.1 * values['level']);
+    break;
+  case 'sorcerer':
+    row_class_hp = Math.floor((values['dex'] -20) * 0.3 * values['level']);
+    break;
+  case 'enchanter':
+    row_class_hp = Math.floor((values['dex'] -20) * 0.3 * values['level']);
+    break;
+  case 'rune':
+    row_class_hp = Math.floor((values['dex'] -20) * 0.3 * values['level']);
+    break;
+  case 'priest':
+    row_class_hp = Math.floor((values['con'] -20) * 0.2 * values['level']);
+    break;
+  case 'cleric':
+    row_class_hp = Math.floor((values['con'] -20) * 0.2 * values['level']);
+    break;
+  case 'life':
+    row_class_hp = Math.floor((values['dex'] -20) * 0.2 * values['level']);
+    break;
+  default: //do nothing
+  }
+  $('#resul').text(base_hp + row_class_hp);
+}
+
+$(document).ready(function() {
+  $("input[type='text']").change( function() {
+    calculateHP();
+  });
+  $('select').change( function() {
+    calculateHP();
+  });
+});
