6c6ff577c3ebbe7d6ef2edac06b26535462528a9
MrBongiolo <ralfsb2008@gmail.com>
2011-02-10 15:57:48 -0400

    updated commentaries

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,6 +2,8 @@
 //Ichzilla Javascript file
 //
 
+//---- HP ----//
+//
 function calculateHP() {
   var values = {};
   $.each($('form').serializeArray(), function(i, field) {
@@ -49,19 +51,28 @@ function calculateHP() {
   case 'cleric':
     row_class_hp = Math.floor((values['con'] -20) * 0.2 * values['level']);
     break;
+//
   case 'life':
     row_class_hp = Math.floor((values['dex'] -20) * 0.2 * values['level']);
     break;
   default: //do nothing
   }
   $('#status_hp').text(base_hp + row_class_hp);
+//
 }
 
-//Formula (atleast for caster classes):
+//---- MAGIC POWER ----//
 //
+//Formula:
+//  
+//  Caster classes:
 //  magic = ((INT - 20) * 3.1) + ((WIS - 20) * 2)
 //
+//  Fighter classes:
+//  magic = ((INT - 20) * 2) + ((WIS - 20) * 2)
+//
 //source: http://forum.ruinsofwar.com/viewtopic.php?f=27&t=6719
+//
 function calculateMagicPower() {
   var values = {};
   $.each($('form').serializeArray(), function(i, field) {
@@ -115,6 +126,74 @@ function calculateMagicPower() {
     //do nothing
   }
   $('#status_magic_power').text(row_class_magic);
+}
+
+//---- MAGIC RESISTANCE ----//
+//
+//Formula:
+//
+//  Fighter classes:
+//  mr = ((INT - 20) * 0.75) + ((WIS - 20) * 0.75)
+//
+//create function to calculate it
+
+
+
+//---- start calculation ----//
+//
+//Function used to call all calculation logic
+//
+function calculate() {
+  var values = {};
+  $.each($('form').serializeArray(), function(i, field) {
+    values[field.name] = field.value;
+  });
+  switch(values['row_class']) {
+  case 'warrior':
+    row_class_magic = Math.floor(((values['int'] - 20) * 3.1) + ((values['wis'] - 20) * 2));
+    break;
+  case 'attacker':
+    row_class_magic = Math.floor(((values['int'] - 20) * 3.1) + ((values['wis'] - 20) * 2));
+    break;
+  case 'defender':
+    row_class_magic = Math.floor(((values['int'] - 20) * 3.1) + ((values['wis'] - 20) * 2));
+    break;
+  case 'templar':
+    row_class_magic = Math.floor(((values['int'] - 20) * 3.1) + ((values['wis'] - 20) * 2));
+    break;
+  case 'assassin':
+    row_class_magic = Math.floor(((values['int'] - 20) * 3.1) + ((values['wis'] - 20) * 2));
+    break;
+  case 'archer':
+    row_class_magic = Math.floor(((values['int'] - 20) * 3.1) + ((values['wis'] - 20) * 2));
+    break;
+  case 'gunner':
+    row_class_magic = Math.floor(((values['int'] - 20) * 3.1) + ((values['wis'] - 20) * 2));
+    break;
+  case 'shadow':
+    row_class_magic = Math.floor(((values['int'] - 20) * 3.1) + ((values['wis'] - 20) * 2));
+    break;
+  case 'sorcerer':
+    row_class_magic = Math.floor(((values['int'] - 20) * 3.1) + ((values['wis'] - 20) * 2));
+    break;
+  case 'enchanter':
+    row_class_magic = Math.floor(((values['int'] - 20) * 3.1) + ((values['wis'] - 20) * 2));
+    break;
+  case 'rune':
+    row_class_magic = Math.floor(((values['int'] - 20) * 3.1) + ((values['wis'] - 20) * 2));
+    break;
+  case 'priest':
+    row_class_magic = Math.floor(((values['int'] - 20) * 1) + ((values['wis'] - 20) * 1.5));
+    break;
+  case 'cleric':
+    row_class_magic = Math.floor(((values['int'] - 20) * 1) + ((values['wis'] - 20) * 1.5));
+    break;
+  case 'life':
+    row_class_magic = Math.floor(((values['int'] - 20) * 1) + ((values['wis'] - 20) * 1.5));
+    break;
+  default:
+    //do nothing
+  }
 }
 
 $(document).ready(function() {
