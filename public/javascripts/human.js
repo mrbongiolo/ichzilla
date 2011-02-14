1834c39948504303f095e282f811f4bd71b5fdbf
MrBongiolo <ralfsb2008@gmail.com>
2011-02-13 23:49:50 -0400

    Started new JS foror RowClasses (base class) and Human Classes

---
+++ public/javascripts/human.js
@@ -0,0 +1,61 @@
+function Enchanter(lvl, str, con, dex, inte, wis) {
+  RowClass.call(this, lvl, str, con, dex, inte, wis);
+}
+
+Enchanter.prototype = new RowClass( );
+
+delete Enchanter.prototype.lvl;
+delete Enchanter.prototype.str;
+delete Enchanter.prototype.con;
+delete Enchanter.prototype.dex
+delete Enchanter.prototype.inte;
+delete Enchanter.prototype.wis;
+delete Enchanter.prototype.bonusPoints;
+
+Enchanter.prototype.constructor = RowClass;
+
+//
+//---- Overridding Some Methods
+//
+
+Enchanter.prototype.maxDex = function( ) {
+  return this.dex + (this.bonusPoints / 2);
+}
+
+Enchanter.prototype.maxInte = function( ) {
+  return this.inte + (this.bonusPoints / 2);
+}
+
+Enchanter.prototype.calculateAndSetDex = function( ) {
+  if(this.lvl < 10) {
+    this.lvl = this.lvl - 1 + 20;
+  } else {
+    var i = 1;
+    this.dex = 20;
+    for(i = 1; i < this.lvl; i++) {
+      if(i % 2 == 0) {
+        this.dex = this.dex + 1;
+      } 
+      if(i % 2 == 1) {
+        this.dex = this.dex + 2;
+      }
+    }
+  }
+}
+
+Enchanter.prototype.calculateAndSetInte = function( ) {
+  if(this.lvl < 10) {
+    this.inte = 20;
+  } else {
+    var i = 1;
+    this.inte = 20;
+    for(i = 1; i < this.lvl; i++) {
+      if(i % 2 == 0) {
+        this.inte = this.inte + 2;
+      }
+      if(i % 2 == 1) {
+        this.inte = this.inte + 1;
+      }
+    }
+  }
+}
---
+++ public/javascripts/row_class.js
@@ -0,0 +1,66 @@
+function RowClass(lvl, str, con, dex, inte, wis) {
+  this.lvl = lvl;
+  this.str = str;
+  this.con = con;
+  this.dex = dex;
+  this.inte = inte;
+  this.wis = wis;
+  this.bonusPoints = 0;
+  this.calculateAndSetBonusPoints( );
+}
+
+RowClass.prototype.calculateAndSetBonusPoints = function( ) {
+  if(this.lvl < 10) {
+    this.bonusPoints = 0;
+  } else {
+    this.bonusPoints = (((this.lvl -1) * 4) + 10);
+  }
+}
+
+//
+//---- Calculate MAX Stat Value ----//
+//
+
+RowClass.prototype.maxStr = function( ) {
+  return this.str + this.bonusPoints;
+}
+
+RowClass.prototype.maxCon = function( ) {
+  return this.con + this.bonusPoints;
+}
+
+RowClass.prototype.maxDex = function( ) {
+  return this.dex + this.bonusPoints;
+}
+
+RowClass.prototype.maxInte = function( ) {
+  return this.inte + this.bonusPoints;
+}
+
+RowClass.prototype.maxWis = function( ) {
+  return this.wis + this.bonusPoints;
+}
+
+//
+//---- Calculate Stat Values ----//
+//
+
+RowClass.prototype.calculateAndSetStr = function( ) {
+  this.str = 20;
+}
+
+RowClass.prototype.calculateAndSetCon = function( ) {
+  this.con = 20;
+}
+
+RowClass.prototype.calculateAndSetDex = function( ) {
+  this.dex = 20;
+}
+
+RowClass.prototype.calculateAndSetInte = function( ) {
+  this.inte = 20;
+}
+
+RowClass.prototype.calculateAndSetWis = function( ) {
+  this.wis = 20;
+}
