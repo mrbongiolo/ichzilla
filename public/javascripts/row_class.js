58017d7547c312c5047ea0a5407d6d1556b2a623
Alis4311 <Alis4311@gmail.com>
2011-04-17 17:23:56 -0700

    Base HP calculation

--- public/javascripts/row_class.js
+++ public/javascripts/row_class.js
@@ -146,7 +146,7 @@ RowClass.prototype.calculateStats = function( ) {
 //
 //source: http://forum.ruinsofwar.com/viewtopic.php?f=27&t=6719
 //
-//this is the Base HP calculation
+//this is the Base HP calculation. This formula should suffice, since java has */. operater precedence over +- 
 RowClass.prototype.calculateHP = function( ) {
   return Math.floor(150 + 25 * this.lvl + ((this.con-20) * 0.15 * this.lvl) + ((this.dex-20) * 0.05 * this.lvl));
 }
